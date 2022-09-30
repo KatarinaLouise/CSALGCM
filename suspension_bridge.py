ac3941189776dae27775ff1ce454dbfc234b5111
KatarinaLouise <67191957+KatarinaLouise@users.noreply.github.com>
2022-09-30 18:01:00 +0800

    Add files via upload

---
+++ suspension_bridge.py
@@ -0,0 +1,8 @@
+import math
+##suspension bridges pseudocode
+##compare a+s and a*cosh(d/2a)
+## min a is 0, max a is pi*1000
+##low + (high-low) / 2  -> soltion for a
+##plug in mid to 1st formula
+##comparison for absolute error
+##then 2nd formula
