bc88aa91239e7b093ea2772062ac60a03bb59710
julia-dlsu <julianne_deveyra@dlsu.edu.ph>
2022-10-01 11:38:05 +0800

    correct with test case but not accepted solution

--- suspension_bridge.py
+++ suspension_bridge.py
@@ -1,8 +1,29 @@
 import math
-##suspension bridges pseudocode
-##compare a+s and a*cosh(d/2a)
-## min a is 0, max a is pi*1000
-##low + (high-low) / 2  -> soltion for a
-##plug in mid to 1st formula
-##comparison for absolute error
-##then 2nd formula
+import sys
+
+def check_a(d, s):
+    low = 0
+    high = sys.maxsize
+
+    # for bisection of the value of "a"
+    while high - low > 0.0000000001:
+        a = low + (high - low) / 2
+        rhs = a + s
+        lhs = a * math.cosh(d / (2 * a))
+        print("rhs: ", rhs, " lhs: ", lhs)
+
+        if rhs >= lhs:
+            high = a
+        else:
+            low = a
+
+    length = 2 * a * math.sinh(d / (2 * a))
+    length = round(length, 9)
+    return length
+
+
+# main driver
+I = input().split()
+d = int(I[0])
+s = int(I[1])
+print(check_a(d, s))
